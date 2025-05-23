186f4cad81d0b3de31c7f08506e38ff6ee7bb1cd
Jiří Strnadel <Juzba88@gmail.com>
2025-05-23 21:48:03 +0200

    update

--- ToDoList_CoreApi/Areas/Editor/Pages/Edit.cshtml.cs
+++ ToDoList_CoreApi/Areas/Editor/Pages/Edit.cshtml.cs
@@ -34,8 +34,8 @@ namespace ToDoList_CoreApi.Areas.Editor.Pages
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+      
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
