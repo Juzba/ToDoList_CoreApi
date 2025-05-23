fc6756e446a7540b1a133007fec8d77a031c1343
Jiří Strnadel <Juzba88@gmail.com>
2025-05-23 21:29:16 +0200

    Working for each user separate

--- ToDoList_CoreApi/Areas/Editor/Pages/Index.cshtml.cs
+++ ToDoList_CoreApi/Areas/Editor/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ToDoList_CoreApi.Data;
 using ToDoList_CoreApi.Models;
 
 namespace ToDoList_CoreApi.Areas.Editor.Pages
 {
-    public class IndexModel(ApplicationDbContext context) : PageModel
+    public class IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager ) : PageModel
     {
         private readonly ApplicationDbContext _context = context;
+        private readonly UserManager<IdentityUser> _userManager = userManager;
 
         public IList<TaskModel> TaskModel { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            //TaskModel = await _context.Tasks.ToListAsync();
-            TaskModel = await _context.Tasks.OrderBy(p => p.IsCompleted).ThenBy(p=>p.DueDate).ToListAsync();
+            TaskModel = await _context.Tasks
+                .Where(p=>p.UserId == _userManager.GetUserId(User))
+                .OrderBy(p => p.IsCompleted)
+                .ThenBy(p=>p.DueDate)
+                .ToListAsync();
         }
     }
 }
